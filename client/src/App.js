e66c9c916b9530571f6a494c4021a1afa8191420
PercTruman <drshawnhart@gmail.com>
2023-01-19 20:21:05 -0600

    change back end scripts

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './Pages/SIgnup';
+import Signin from './Pages/Signin';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
   	<Routes>
-				{/* <Route path='/' element={<Welcome />} />
-				<Route path='/-login' element={<Login />} /> */}
+			
+				<Route path='/-signin' element={<Signin />} />
 				<Route path='/-signup' element={<Signup />} />
 			
 			</Routes>
